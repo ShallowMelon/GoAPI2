ee1039e5bf206f7640b570b86e51400f7c73f44a
ShallowMelon <wonovincent6@gmail.com>
2025-11-17 21:34:22 +0700

    123

---
+++ cmd/api/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/avukadin/goapi/internal/handlers"
+	"github.com/go-chi/chi"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.SetReportCaller(true)
+	var r *chi.Mux = chi.NewRouter()
+	handlers.Handler(r)
+
+	fmt.Println("Starting GoAPI Services...")
+
+	fmt.Println("Welcome to GoAPI")
+
+	err := http.ListenAndServe("localhost:8080", r)
+	if err != nil {
+		log.Error(err)
+	}
+}
